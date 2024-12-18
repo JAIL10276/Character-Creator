03422831c127c6173e93ccdb0d06601ed66aa693
JAIL10276 <jai5143@psu.edu>
2024-12-17 21:20:22 -0500

    few tweaks

--- rpg-me.js
+++ rpg-me.js
@@ -6,7 +6,7 @@ import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 import 'wired-elements';
-import "@haxtheweb/rpg-character/src/rpg-character.js";
+import "@haxtheweb/rpg-character/rpg-character.js";
 /**
  * `rpg-character`
  *
